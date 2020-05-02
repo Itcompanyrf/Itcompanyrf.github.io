8de9fb375297a2286c366962783f40f8e5a36a8d
Milliard2020year <khaziev.elnar@yandex.ru>
2020-05-02 11:29:27 +0200

    mysite commit

---
+++ MySite/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $(".hamburger").on("click", function () {
+      $(".header__menu").addClass("active");
+    });
+  
+    $(".close-btn").on("click", function () {
+      $(".header__menu").removeClass("active");
+    });
+
+    $(".menu__link").click(function () {
+      $(".menu__link").removeClass("active");
+      $(this).addClass("active");
+    });
+  });
+  $(function () {
+    $('a[data-target^="anchor"]').bind("click.smoothscroll", function () {
+      var target = $(this).attr("href"),
+        bl_top = $(target).offset().top;
+      $("body, html").animate({ scrollTop: bl_top }, 700);
+      return false;
+    });
+  
+    AOS.init();
+  
+    $(window).on("load", function () {
+      $(".loader").fadeOut();
+      $("#preloder").delay().fadeOut("slow");
+    });
+  });
