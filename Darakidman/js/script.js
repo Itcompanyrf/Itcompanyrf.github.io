df837683f8235e4c25bbde8fd9e4377574605523
Itcompanyrf <62737457+Itcompanyrf@users.noreply.github.com>
2021-02-17 21:58:29 +0500

    Darakidman-site

---
+++ Darakidman/js/script.js
@@ -0,0 +1,13 @@
+// $('.portfolio__slider').slick({
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 3,
+//   centerMode: true,
+//   variableWidth: true
+// });
+
+// $('.feedback__slider').slick({
+//   infinite: true,
+//   dots: true,
+//   arrows: false
+// });
